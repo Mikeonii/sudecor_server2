48e272351a8f77e62ee34b6e642d036f533cf8b3
Mikeonii <janmichael_0123@outlook.com>
2021-01-20 22:09:34 +0800

    init

---
+++ database/migrations/2021_01_20_140333_create_attendance_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttendanceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendance', function (Blueprint $table) {
+            $table->id();
+            $table->string('client_id');
+            $table->string('time_in');
+            $table->string('time_out');
+            $table->integer('regular_hour');
+            $table->integer('over_time');
+            $table->integer('sunday');
+            $table->integer('holiday');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendance');
+    }
+}
